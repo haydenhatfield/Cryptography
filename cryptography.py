2ef91b542253cb5543a0a462f251037890a9ee96
haydenhatfield <haydenhatfield@hanovernorwichschools.org>
2016-05-03 14:43:46 -0400

    Updated from Brython Server: 5/3/2016 2:43:46 PM

--- cryptography.py
+++ cryptography.py
@@ -17,7 +17,7 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 i = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 if(i != e or i != d or i != q):
     print("Did not understand command, try again.")
-if(i = e):
+if(i == e):
     me = input("Message: ")
     ke = input("Key: ")
 if(i = d):
